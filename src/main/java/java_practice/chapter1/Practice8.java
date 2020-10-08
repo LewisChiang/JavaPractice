2ec795ae7b738998bd3fda406f15344a3a8bf3c5
LewisChiang <18364298130@163.com>
2020-10-08 16:28:54 +0800

    Merge branch 'master' of https://github.com/LewisChiang/JavaPractice
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/compiler.xml
    #	.idea/misc.xml

---
+++ src/main/java/java_practice/chapter1/Practice8.java
@@ -0,0 +1,15 @@
+package java_practice.chapter1;
+
+/**
+ * @author Lewis
+ * @time 2020-10-08 16:22:25
+ */
+
+public class Practice8 {
+    public static void main(String[] args) {
+        double  perimeter = 2 * 5.5 * Math.PI;
+        double area = 5.5 * 5.5 * Math.PI;
+        System.out.println(perimeter);
+        System.out.println(area);
+    }
+}
